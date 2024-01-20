a23eb2110af4f833b102f8f342d4a4a3616f2ae9
David Delassus <david.jose.delassus@gmail.com>
2024-01-20 22:56:16 +0100

    :bento: :construction: add draft of spritesheet and first level

---
+++ assets/tilemaps/spritesheet.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="spritesheet" tilewidth="32" tileheight="32" tilecount="4" columns="1">
+ <image source="../spritesheet.png" width="32" height="128"/>
+ <tile id="0" type="block.aseprite"/>
+ <tile id="1" type="ground-tile.aseprite"/>
+ <tile id="2" type="ice-block.aseprite"/>
+ <tile id="3" type="water.aseprite"/>
+</tileset>
