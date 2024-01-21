506bfacfa32ed38bb237ac791ce3c56ac08aeac0
David Delassus <david.jose.delassus@gmail.com>
2024-01-22 00:13:48 +0100

    :sparkles: load first level

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <trollworks.hpp>
 #include <trollworks-backend-sdl.hpp>
 #include <game/assets/loader.hpp>
+#include <game/scenes/level.hpp>
+
+using namespace entt::literals;
+
+struct game_state {
+  void setup(tw::controlflow&) {
+    tw::scene_manager::main().load(game::scenes::level{"level1"_hs});
+  }
+};
 
 int SDL_main(int, char** argv) {
   SDL_Log("ARGV0=%s", argv[0]);
@@ -15,12 +24,15 @@ int SDL_main(int, char** argv) {
     .backend = backend
   };
 
+  auto gs = game_state{};
+
   auto loop = tw::game_loop{}
     .with_fps(60)
     .with_ups(50)
     .with_backend(backend)
     .on_setup<&game::assets::resource_loader::setup>(loader)
-    .on_teardown<&game::assets::resource_loader::teardown>(loader);
+    .on_teardown<&game::assets::resource_loader::teardown>(loader)
+    .on_setup<&game_state::setup>(gs);
 
   loop.run();
   return 0;
